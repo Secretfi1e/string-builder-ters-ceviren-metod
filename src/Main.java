34944a50dacf8e530409de22e8ac6c33184bd9ba
Secretfi1e <beyzadeemin9@gmail.com>
2024-05-04 19:46:47 +0400

    stringbuilder ters ceviren metod

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        StringBuilder stringBuilder = new StringBuilder("beyzadeemin");
+        for(int i=stringBuilder.length()-1;i>=0;i--){
+            System.out.print(stringBuilder.charAt(i));
+
+        }
+
+    }
+}
